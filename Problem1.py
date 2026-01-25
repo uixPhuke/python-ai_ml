2f5385447e7ce0fe3161ff451d968e61013e10b6
uixPhuke <uixphuke@gmail.com>
2026-01-26 00:36:54 +0530

    Problem 1 solved

---
+++ Problem1.py
@@ -0,0 +1,44 @@
+students = [
+    ("Arka", "Mathematics"),
+    ("Riya", "Computer Science"),
+    ("Sayan", "Maths"),
+    ("Arka", "Biology"),
+    ("Riya", "Computer Science"),
+    ("Sayan", "Physics"),
+    ("Ananya", "Chemistry"),
+    ("Rahul", "English"),
+    ("Priya", "Biology")
+]
+
+#to access all name
+for tup in students:
+    print(tup[0])
+
+print("\n\n")
+#to access all subjects
+for tup in students:
+    print(tup[1])
+
+#to access both
+for name,course in students:
+    print(name," : ",course)
+
+#adding name to set --> will be unique
+students_set=set()
+
+for tup in students:
+    students_set.add(tup[0])
+    #adding name
+
+print(students_set)
+
+#adding  to set --> will be unique
+studentsCourse_set=set()
+
+for tup in students:
+    studentsCourse_set.add(tup[1])
+    #adding name
+
+print(studentsCourse_set)
+
+
