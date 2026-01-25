51feaca12497f886297b413980cf63e0dec20e1f
uixPhuke <uixphuke@gmail.com>
2026-01-25 21:50:39 +0530

    tuple

---
+++ Tuple.py
@@ -0,0 +1,10 @@
+tuple1=(1,2,3,"qbcd")
+print(tuple1)
+print(type(tuple1))
+
+tuple2=(1)#not possible. because python take it as a mathematical expression
+#taking as a int value
+print(type(tuple2))
+
+tuple3=(1,)#comma reqd.
+print(type(tuple3))
