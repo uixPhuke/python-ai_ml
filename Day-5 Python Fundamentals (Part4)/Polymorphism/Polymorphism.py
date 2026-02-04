762d78972e1723f864baebc62b5d1265b0989b3b
uixPhuke <uixphuke@gmail.com>
2026-02-04 23:45:09 +0530

    Concept of Polymorphism

---
+++ Day-5 Python Fundamentals (Part4)/Polymorphism/Polymorphism.py
@@ -0,0 +1,16 @@
+#Polymorphism Means ---->> More Forms
+#Multiple Function---->> Same Name (But all are Different Function/They have Different Forms)
+
+#Eg. of Polymorphism is + operator(Operator Overloading)
+#+(plus) is used to add two integers
+#Also do concradinate Two String
+
+print(7+10 , "Hello"+"World")
+
+
+#Types:
+#.  1. Function Overriding(it involves Inheritance)
+#        Redefining Parent's Class Function --> in Child's Class (With Same Name)
+#.  2.Duck Typing
+
+#Funcyion 
