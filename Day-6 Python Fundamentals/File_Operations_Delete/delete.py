0dbd0d98496ef219714f46943d9bdd70bf425c46
uixPhuke <uixphuke@gmail.com>
2026-05-16 22:48:15 +0530

    Add delete.py for file deletion demonstration and sample.txt as a target file

---
+++ Day-6 Python Fundamentals/File_Operations_Delete/delete.py
@@ -0,0 +1,3 @@
+import os
+
+os.remove("sample.txt") #os.remove() method is used to delete a file. It takes the file name as an argument and deletes the file from the file system. It is important to note that once a file is deleted, it cannot be recovered, so use this method with caution.
